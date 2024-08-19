301e966d49f58ea98e9a850aeca23d59b484ef24
Fearioh <alexis_delattre@outlook.fr>
2024-08-19 12:32:00 +0200

    Premier push, install fini manque la ligne d'installation

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+YELLOW="\033[1;33m"
+BLUE="\033[1;34m"
+PURPLE="\033[1;35m"
+GREEN="\033[1;32m"
+RED="\033[1;31m"
+RESET="\033[0m"
+
+ls $HOME/Documents -R | grep "poolparty.sh" > /dev/null
+value=$?
+if [[ $value -eq 0 ]]; then
+	echo -e "\n${RED}poolparty.sh existe déjà dans vos Documents${RESET}\n"
+	exit -1
+else
+	ls $HOME -Ra | grep ".zshrc" > /dev/null
+	value=$?
+	if [[ $value -ne 0 ]]; then
+		sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	fi
+	git clone https://github.com/Fearioh/PoolParty.git $HOME/Documents/script/PoolParty
+	echo "alias pp='sh $HOME/Documents/script/PoolParty/poolparty.sh'" >> $HOME/.zshrc
+	zsh -c 'source $HOME/.zshrc'
+fi
+
+echo -e "\n${GREEN}L'installation de PoolParty est complète, tapez "pp" dans votre terminal pour l'utiliser !${RESET}\n"
