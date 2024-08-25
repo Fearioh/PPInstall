e8b0340124a4a48ada4d13a6dfe3d031ace612b2
Fearioh <alexis_delattre@outlook.fr>
2024-08-25 10:46:25 +0200

    /ADD Version finale de l'installateur de PP

--- install.sh
+++ install.sh
@@ -13,14 +13,15 @@ if [[ $value -eq 0 ]]; then
 	echo -e "\n${RED}poolparty.sh existe déjà dans vos Documents${RESET}\n"
 	exit -1
 else
-	ls $HOME -Ra | grep ".zshrc" > /dev/null
+	ls $HOME -Ra | grep -q "oh-my-zsh.sh" ~/.zshrc > /dev/null
 	value=$?
 	if [[ $value -ne 0 ]]; then
-		sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+		sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 	fi
 	git clone https://github.com/Fearioh/PoolParty.git $HOME/Documents/script/PoolParty
-	echo "alias pp='sh $HOME/Documents/script/PoolParty/poolparty.sh'" >> $HOME/.zshrc
-	zsh -c 'source $HOME/.zshrc'
+	echo "alias pp='bash $HOME/Documents/script/PoolParty/poolparty.sh'" >> $HOME/.zshrc
+	zsh -c "source $HOME/.zshrc"
+	exec zsh
 fi
 
 echo -e "\n${GREEN}L'installation de PoolParty est complète, tapez "pp" dans votre terminal pour l'utiliser !${RESET}\n"
